5a53e74b45769b84b3c216f104509304beee4316
SummifyDE <Summify.de@gmail.com>
2025-05-14 16:15:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Supabase Konfiguration – Ersetze durch deine eigenen Keys
+const { createClient } = supabase;
 const supabase = window.supabase.createClient(
   'https://lyioruosnltgowlxluon.supabase.co',  // Deine Supabase-URL
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5aW9ydW9zbmx0Z293bHhsdW9uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMjQ0NjEsImV4cCI6MjA2MjgwMDQ2MX0.rC-3plAsVFX91nbxeDFVDUFYSzwCtBBkqoNBDVL5amI'                       // Dein Anon Key
