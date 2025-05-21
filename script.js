017572ef6c94e83cfa64f26aeaf8415ab6bdb690
SummifyDE <Summify.de@gmail.com>
2025-05-21 14:53:01 +0200

    Update script.js co bei url angepasst

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // === Supabase Konfiguration ===
-const SUPABASE_URL = 'https://lyioruosnltgowlxluon.supabase.coo';        // ← HIER deine Supabase-URL einfügen
+const SUPABASE_URL = 'https://lyioruosnltgowlxluon.supabase.co';        // ← HIER deine Supabase-URL einfügen
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5aW9ydW9zbmx0Z293bHhsdW9uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMjQ0NjEsImV4cCI6MjA2MjgwMDQ2MX0.rC-3plAsVFX91nbxeDFVDUFYSzwCtBBkqoNBDVL5amI';                   // ← HIER dein Anon-Key einfügen
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
