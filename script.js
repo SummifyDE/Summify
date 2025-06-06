f2c3406c2a16a7d018f792e75f0052455daefb27
SummifyDE <Summify.de@gmail.com>
2025-06-06 13:11:59 +0200

    Update script.js doppelte key angabe gelöscht

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // === Supabase Konfiguration ===Add commentMore actions
 const SUPABASE_URL = 'https://lyioruosnltgowlxluon.supabase.co';        
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'; // gekürzt
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5aW9ydW9zbmx0Z293bHhsdW9uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMjQ0NjEsImV4cCI6MjA2MjgwMDQ2MX0.rC-3plAsVFX91nbxeDFVDUFYSzwCtBBkqoNBDVL5amI'; // gekürzt
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
